9045eeb871a3c4ddb3667f713b887a271beec479
NikiSukiasyan <justplayingcsgo@gmail.com>
2023-07-24 00:44:29 +0400

    feat: Added functionality in AllArticles file and fixed some styles

--- src/components/allarticles/AllArticles.jsx
+++ src/components/allarticles/AllArticles.jsx
@@ -1,13 +1,22 @@
-import React from "react";
-import { useActiveCategory } from "../main/Main";
+import React, { useState } from "react";
 import "../main/Main.scss";
 import "./AllArticles.scss";
 import { useArticlesContext } from "../../ArticlesContext";
 import Footer from "../footer/Footer";
 
 function AllArticles() {
-  const { activeCategory, handleCategoryClick } = useActiveCategory();
   const { articles } = useArticlesContext();
+
+  const [activeCategory, setActiveCategory] = useState("All");
+
+  const filteredArticles =
+    activeCategory === "All"
+      ? articles
+      : articles.filter((article) => article.type === activeCategory);
+
+  const handleCategoryClick = (category) => {
+    setActiveCategory(category);
+  };
 
   return (
     <>
@@ -52,26 +61,32 @@ function AllArticles() {
       </ul>
       <p className="view-all">View All</p>
       <div className="whole-articles-container">
-        {articles.map((article) => (
-          <div className="every-article" key={article.id}>
-            <img src={article.image} alt="rume" />
-            <div className="blog-type">
-              <p>{article.type}</p>
-            </div>
-            <span>{article.date}</span>
-            <div>
-              <p className="every-title">{article.title}</p>
-              <span className="every-description">{article.description}</span>
-            </div>
-            <div className="profile-information-container">
-              <img src={article.profile} />
-              <div className="job-information">
-                <p>{article.name}</p>
-                <span>{article.job}</span>
+        {filteredArticles.length > 0 ? (
+          filteredArticles.map((article) => (
+            <div className="every-article" key={article.id}>
+              <img src={article.image} alt="rume" />
+              <div className="blog-type">
+                <p>{article.type}</p>
+              </div>
+              <span>{article.date}</span>
+              <div>
+                <p className="every-title">{article.title}</p>
+                <span className="every-description">{article.description}</span>
+              </div>
+              <div className="profile-information-container">
+                <img src={article.profile} alt={article.name} />
+                <div className="job-information">
+                  <p>{article.name}</p>
+                  <span>{article.job}</span>
+                </div>
               </div>
             </div>
+          ))
+        ) : (
+          <div className="all-articles-no-results">
+            <p>No Results Found :(</p>
           </div>
-        ))}
+        )}
       </div>
       <Footer />
     </>
